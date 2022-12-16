28fb9f3517eb2fcb46fb96eddb9a73e082971e50
daniel gerez <danielgerez999@hotmail.com>
2022-12-16 14:24:38 -0300

    platround() cambio if

--- RockPaperScissors/main.js
+++ RockPaperScissors/main.js
@@ -1,5 +1,3 @@
-
-
 function getComputerChoice (){
     let numero = Math.floor(Math.random()*3);
     let choice = ' ';
@@ -20,48 +18,48 @@ function getComputerChoice (){
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
 
-    if (playerSelection == "rock"){
-        if (computerSelection == "rock"){
+    if (playerSelection == "rock" && computerSelection == "rock" ||
+        playerSelection == "paper" && computerSelection == "paper" ||
+        playerSelection == "scissors" && computerSelection == "scissors"){
+        
         console.log("empate")
-        }
-        else if (computerSelection == "scissors"){
-            console.log("punto jugador")
-        }
-        else{
-            console.log("punto compudora")
-        }
+    
     }
-    else if (playerSelection == "paper"){
-        if (computerSelection == "paper"){
-        console.log("empate")
-        }
-        else if (computerSelection == "rock"){
-            console.log("punto jugador")
-        }
-        else{
-            console.log("punto compudora")
-        }
+    else if (playerSelection == "paper" && computerSelection == "rock" ||
+             playerSelection == "rock" && computerSelection == "scissors" ||
+             playerSelection == "scissors" && computerSelection == "paper"){
+   
+        console.log("punto jugador")
+    
+    
     }
-    else if (playerSelection == "scissors"){
-        if (computerSelection == "scissors"){
-        console.log("empate")
-        }
-        else if (computerSelection == "paper"){
-            console.log("punto jugador")
-        }
-        else{
+    else if (playerSelection == "paper" && computerSelection == "scissors" ||
+             playerSelection == "rock" && computerSelection == "paper" ||
+             playerSelection == "scissors" && computerSelection == "rock"){
+   
             console.log("punto compudora")
-        }
+        
     }
     else{
         console.log("escribir bien => rock, paper or scissors")
     }
 
     return "computadora: "+ computerSelection + " | "+"player: " + playerSelection;
+};
+
+
+function game (){
+let player = 0;
+let computer = 0;
+
+for (let index = 0; index < 5; index++) {
+    let computerSelection = getComputerChoice ();
+    
+    let playerSelection = prompt("rock, paper or scissors");
+    console.log(playRound(playerSelection, computerSelection))
+  
 }
 
-let computerSelection = getComputerChoice ();
-    
-let playerSelection = prompt("rock, paper or scissors");
-console.log(playRound(playerSelection, computerSelection))
+}
 
+game()
